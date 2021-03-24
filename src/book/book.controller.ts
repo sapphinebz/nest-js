584ccb5e01de8b4c32aaae213936af836b16b0b0
sapphinebz <sapphinebz@gmail.com>
2021-03-24 15:14:47 +0700

    hotFix

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,9 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { Book } from './book';
 
 @Controller('book')
 export class BookController {
   @Get()
-  findBookAll(): string {
-    return '';
+  findBookAll(): Book[] {
+    return [
+      {
+        bookId: 0,
+        bookName: 'BookName0',
+      },
+      {
+        bookId: 1,
+        bookName: 'BookName1',
+      },
+    ];
   }
 }
