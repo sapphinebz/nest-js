afe0bc46f72ddd9b54c50b5a2b013e6d99014cde
sapphinebz <sapphinebz@gmail.com>
2021-04-08 09:59:44 +0700

    add new feature

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -13,7 +13,6 @@ export class BookController {
 
   @Get('promise')
   async getBooksPromise(): Promise<Book[]> {
-    // test promise // test //test
     const books = await this.bookService.readBooksPromise();
     return books;
   }
